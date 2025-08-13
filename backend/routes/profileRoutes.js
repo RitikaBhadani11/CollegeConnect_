8ecd84b86669a68a2e0743b429b225af001af7b8
RitikaBhadani11 <ritikabhadani98@gmail.com>
2025-08-14 01:36:51 +0530

    Done

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authMiddleware } = require('../middleware/authMiddleware');
+const { authMiddleware } = require('../middleware/authmiddleware');
 const profileController = require('../controllers/profileController');
 const upload = require('../middleware/uploadMiddleware');
 
